c03f1fd1e6b8fcd3b605cf69661081448ad5c925
Mauricio Melo Almeida <mauricio.melo.almeida@gmail.com>
2016-11-20 15:52:04 -0200

    included Dashboard activity

--- app/src/main/java/com/mauricioalmeida/travelbudget/LoginActivity.java
+++ app/src/main/java/com/mauricioalmeida/travelbudget/LoginActivity.java
@@ -1,13 +1,36 @@
 package com.mauricioalmeida.travelbudget;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText password;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        name = (EditText) findViewById(R.id.edt_user);
+        password = (EditText) findViewById(R.id.edt_password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(onClickLogin());
+    }
+
+    private View.OnClickListener onClickLogin() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        };
     }
 }
